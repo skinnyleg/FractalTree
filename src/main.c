96574abd7293d0ba556d0f6d725ca885d12e5fa9
skinnyleg <haitamfox12@gmail.com>
2022-12-01 16:42:06 +0100

    the fractal tree is drawn

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int sl;
 int bpp;
 int end;
 void *img;
-int len = 100;
+int len = 50;
 
 void	pixel_put(int x, int y)
 {
@@ -18,44 +18,42 @@ void	pixel_put(int x, int y)
 }
 void	render_line(double beginx , double beginy, double endx, double endy)
 {
-	int		dx;
-	int		dy;
-	double	ddx;
-	double	ddy;
+	double	dx;
+	double	dy;
 	double	x_inc;
 	double	y_inc;
 	int		step;
 
 	dx = (endx - beginx);
 	dy = (endy - beginy);
-	if (abs(dx) > abs(dy))
-		step = abs(dx);
+	if (abs((int)dx) > abs((int)dy))
+		step = abs((int)dx);
 	else
-		step = abs(dy);
-	x_inc = ((double)dx / (double)step);
-	y_inc = ((double)dy / (double)step);
+		step = abs((int)dy);
+	x_inc = (dx / (double)step);
+	y_inc = (dy / (double)step);
 	dx = round(beginx);
 	dy = round(beginy);
-	ddx = beginx;
-	ddy = beginy;
 	while (step != 0)
 	{
-		pixel_put(dx, dy);
-		ddx += x_inc;
-		ddy += y_inc;
-		dx = round(ddx);
-		dy = round(ddy);
+		pixel_put(round(dx), round(dy));
+		dx += x_inc;
+		dy += y_inc;
 		step--;
 	}
 }
+
 int	ft_close()
 {
 	exit(0);
 	return (0);
 }
+
 int	keypress(int keycode)
 {
 	if (keycode == 53)
+		ft_close();
+	if (keycode == 65307)
 		ft_close();
 	return 0;
 }
@@ -68,26 +66,25 @@ double convert_rad(double degree)
 
 void	ft_drawtree(double angle, double endx, double endy, int level)
 {
-	double tmpendx = endx;
-	double tmpendy = endy;
-	double tmpoldendx;
-	double tmpoldendy;
-	len -= 10;
-	double newbeginx = endx;
-	double newbeginy = endy;
-	tmpoldendx = newbeginx - cos(angle) * len;
-	tmpoldendy = newbeginy - sin(angle) * len;
-	render_line(newbeginx, newbeginy, tmpoldendx, tmpoldendy);
-	newbeginx = tmpendx;
-	newbeginy = tmpendy;
-	endx = newbeginx + cos(-angle) * len;
-	endy = newbeginy + sin(-angle) * len;
-	render_line(newbeginx, newbeginy, endx, endy);
-	angle -= convert_rad(1.5);
-	if (level == 0)
-		return ;
-	ft_drawtree(angle, tmpoldendx, tmpoldendy, level - 1);
-	ft_drawtree(angle, endx, endy, level - 1);
+	double	leftendx;
+	double	leftendy;
+	double	rightendx;
+	double	rightendy;
+	double	newbeginx;
+	double	newbeginy;
+
+	if (len < 0 || level == 0)
+		return;
+	newbeginx = endx;
+	newbeginy = endy;
+	leftendx = newbeginx - cos(angle) * len;
+	leftendy = newbeginy - sin(angle) * len;
+	render_line(newbeginx, newbeginy, leftendx, leftendy);
+	rightendx = newbeginx + cos(-angle) * len;
+	rightendy = newbeginy + sin(-angle) * len;
+	render_line(newbeginx, newbeginy, rightendx, rightendy);
+	ft_drawtree(angle, leftendx, leftendy, level - 1);
+	ft_drawtree(angle, rightendx, rightendy, level - 1);
 }
 
 int main()
@@ -100,9 +97,9 @@ int main()
 		exit(1);
 	double endx = (double)WIN_WIDTH / 2;
 	double endy = (double)WIN_HEIGHT - (double)len;
-	render_line(WIN_WIDTH / 2, WIN_HEIGHT, endx, endy);
+	render_line((double)WIN_WIDTH / 2, WIN_HEIGHT, endx, endy);
 	double angle = convert_rad(45);
-	ft_drawtree(angle, endx, endy, 3);
+	ft_drawtree(angle, endx, endy, 20);
 	mlx_put_image_to_window(mlx_ptr, mlx_win, img, 0, 0);
 	mlx_hook(mlx_win, 17, 0, ft_close, NULL);
 	mlx_hook(mlx_win, 2, (1L << 0), keypress, NULL);
